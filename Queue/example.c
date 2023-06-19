394f5b402b4bd049943b7a41e45515a869a148fd
Fabian Pfaff <mysterytyp@gmail.com>
2023-06-19 12:36:19 +0200

    blyat

--- Queue/example.c
+++ Queue/example.c
@@ -21,10 +21,13 @@ int main(void)
     myqueue_init(&queue);
     
     myqueue_enqueue(&queue, tmp_data);
+    myqueue_enqueue(&queue, tmp_data);
 
     struct big_ass_struct data_2 = myqueue_dequeue(&queue);
 
     printf("data_2.data1 = %d\n", data_2.data1);
+
+    myqueue_destroy(&queue);
 
     return 0;
 }
